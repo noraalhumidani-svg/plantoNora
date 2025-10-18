adc2ddda684d356830b0f8c3732c5c9a8e768421
noraalhumidani-svg <noraalhumidani@gmail.com>
2025-10-18 14:20:11 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Nora Abdullah Alhumaydani on 26/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantoTests/PlantoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoTests.swift
+//  PlantoTests
+//
+//  Created by Nora Abdullah Alhumaydani on 26/04/1447 AH.
+//
+
+import Testing
+@testable import Planto
+
+struct PlantoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
